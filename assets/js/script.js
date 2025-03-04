aa0a060fa7591758effb127770c4ae4f1864d0fd
gielvangaal <gielvangaal@gmail.com>
2025-03-04 20:01:43 +0100

    feat-start hero

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+  document.querySelectorAll(".card-container").forEach(card => {
+            const container = card.querySelector(".media-card-container");
+            const video = card.querySelector(".card-video");
+            const img = card.querySelector(".card-img");
+
+            container.addEventListener("mouseenter", () => {
+                video.style.display = "block"; // Video tonen
+                video.play();
+                img.style.opacity = "0"; // Afbeelding verbergen (fade-out effect)
+                // video.style.transition = "opacity 3s ease";
+            });
+
+            container.addEventListener("mouseleave", () => {
+                video.pause();
+                video.style.display = "none"; // Video verbergen
+                img.style.opacity = "1"; // Afbeelding tonen (fade-in effect)
+            });
+        });
