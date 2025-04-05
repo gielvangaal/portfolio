54fa4c6d79f7796da00e6e9930012643a8340c0c
gielvangaal <gielvangaal@gmail.com>
2025-04-05 15:50:15 +0200

    FEAT: enhance media queries and styles for improved layout; update background colors and widths for better responsiveness; comment out unused JavaScript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
 //   cards video on hover
-document.querySelectorAll(".card-container").forEach(card => {
-    const container = card.querySelector(".media-card-container");
-    const video = card.querySelector(".card-video");
-    const img = card.querySelector(".card-img");
+// document.querySelectorAll(".card-container").forEach(card => {
+//     const container = card.querySelector(".media-card-container");
+//     const video = card.querySelector(".card-video");
+//     const img = card.querySelector(".card-img");
 
-    container.addEventListener("mouseenter", () => {
-        video.style.display = "block"; // Video tonen
-        video.play();
-        img.style.opacity = "0"; // Afbeelding verbergen (fade-out effect)
-        // video.style.transition = "opacity 3s ease";
-    });
+//     container.addEventListener("mouseenter", () => {
+//         video.style.display = "block"; // Video tonen
+//         video.play();
+//         img.style.opacity = "0"; // Afbeelding verbergen (fade-out effect)
+//         // video.style.transition = "opacity 3s ease";
+//     });
 
-    container.addEventListener("mouseleave", () => {
-        video.pause();
-        video.style.display = "none"; // Video verbergen
-        img.style.opacity = "1"; // Afbeelding tonen (fade-in effect)
-    });
-});
+//     container.addEventListener("mouseleave", () => {
+//         video.pause();
+//         video.style.display = "none"; // Video verbergen
+//         img.style.opacity = "1"; // Afbeelding tonen (fade-in effect)
+//     });
+// });
 
 document.querySelector("#btn-terug").addEventListener("click", function() {
     window.history.back();
