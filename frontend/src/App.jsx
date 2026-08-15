7702e7b6ba575901dc414ac02cd868c4e86b3063
Giel van Gaal <gielvangaal@gmail.com>
2026-08-15 12:36:14 +0200

    start portfolio section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import PageLayout from "./components/layout/PageLayout";
 import HeroSection from "./features/hero/HeroSection";
+import PortfolioSection from "./features/portfolio/PortfolioSection.jsx";
 
 export default function App() {
     const lang = "en"
     return (
         <PageLayout>
             <HeroSection lang={lang} />
+            <PortfolioSection lang={lang} />
         </PageLayout>
     );
 }
---
+++ frontend/src/features/portfolio/PortfolioSection.jsx
@@ -0,0 +1,12 @@
+import "./portfolio.css";
+
+export default function PortfolioSection() {
+    return (
+        <section id="portfolio" className="portfolio-section">
+            <div className="section-heading">
+                <h2>Portfolio</h2>
+                <div className="section-heading__highlight" />
+            </div>
+        </section>
+    );
+}
